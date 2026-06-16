b34dee3be24f098cc429cc586b3c2809d2a09de9
Kevin Damas <kevin.damas.4@gmail.com>
2026-06-16 17:16:03 -0600

    Cambios en axios

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,11 @@ import MaintenancePage from "./pages/Maintenance";
 
 function App() {
 
-  if (
-    import.meta.env.VITE_MAINTENANCE_MODE === "true"
-  ) {
-
+  if (import.meta.env.VITE_MAINTENANCE_MODE === "true") {
     return <MaintenancePage />;
-
   }
 
   return <AppRoutes />;
-
 }
 
 export default App;
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-// src/api/axios.ts
-
 export const api = axios.create({
-  baseURL: "https://sitio-inexistente.com/api"
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Content-Type": "application/json"
+    }
 });
