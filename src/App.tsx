ceffe67b2454115cb497700f9d907de1c45faacc
Kevin Damas <kevin.damas.4@gmail.com>
2026-06-16 17:10:02 -0600

    Pagina de quiebre

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import AppRoutes from "./routes/AppRoutes";
+import MaintenancePage from "./pages/Maintenance";
 
 function App() {
+
+  if (
+    import.meta.env.VITE_MAINTENANCE_MODE === "true"
+  ) {
+
+    return <MaintenancePage />;
+
+  }
+
   return <AppRoutes />;
+
 }
 
 export default App;
---
+++ src/pages/Maintenance.tsx
@@ -0,0 +1,33 @@
+function MaintenancePage() {
+
+    return (
+
+        <div
+            style={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+        >
+
+            <h1>
+                🚧 Servicio temporalmente fuera de servicio
+            </h1>
+
+            <p>
+                Estamos trabajando para restablecer la plataforma.
+            </p>
+
+            <p>
+                Gracias por su comprensión.
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default MaintenancePage;
