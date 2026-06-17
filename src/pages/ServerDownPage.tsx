5ec1b2b1c5ae956d1ec541af80c7aaf920a75e07
Kevin Damas <kevin.damas.4@gmail.com>
2026-06-16 20:30:48 -0600

    Changes on ServerDownPage

---
+++ src/pages/ServerDownPage.tsx
@@ -0,0 +1,10 @@
+function ServerDownPage() {
+    return (
+      <div>
+        <h1>Servicio temporalmente no disponible</h1>
+        <p>Por favor, inténtelo nuevamente más tarde.</p>
+      </div>
+    );
+  }
+  
+  export default ServerDownPage;
